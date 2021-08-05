c0af8533a1b2cb8fc69c26fe84b244e0003310ad
Steve Martinelli <s.martinelli@gmail.com>
2021-08-04 23:33:49 -0400

    remove path bits

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
     description: "A Gatsby theme for the carbon design system",
     keywords: "gatsby,theme,carbon",
   },
-  pathPrefix: "/open-source-cloud-guide-carbon",
   plugins: [
     {
       resolve: "gatsby-plugin-manifest",
