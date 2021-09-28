35ef9d54afe5dab48519e67ca609a83ac6ad4453
Steve Martinelli <s.martinelli@gmail.com>
2021-09-28 15:55:40 -0400

    remove unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: "/open-source-cloud-guide-carbon",
   siteMetadata: {
     title: "Open Source Cloud Guide",
-    description: "Open Source Cloud Guide",
+    description: "This guide is for software developers and technical folks wanting to know the landscape of open source software.",
     keywords: "open source,cloud",
   },
   plugins: [
